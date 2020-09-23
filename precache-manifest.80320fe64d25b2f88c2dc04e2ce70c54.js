983a1a479d5e998d13b6b2f018f27251fbee41bb
Harsh Raj <hr45226@gmail.com>
2020-09-23 21:05:32 +0530

    Updates

---
+++ precache-manifest.80320fe64d25b2f88c2dc04e2ce70c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c19d977052edccee0f5298e8e046e56",
+    "url": "/Live-IPL-Score/index.html"
+  },
+  {
+    "revision": "584044bdc4db3f5e9b2b",
+    "url": "/Live-IPL-Score/static/css/main.bcfe57e8.chunk.css"
+  },
+  {
+    "revision": "bf8cfb9d515515d9f028",
+    "url": "/Live-IPL-Score/static/js/2.cd2bbec0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Live-IPL-Score/static/js/2.cd2bbec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584044bdc4db3f5e9b2b",
+    "url": "/Live-IPL-Score/static/js/main.1696ab54.chunk.js"
+  },
+  {
+    "revision": "1fcdd8d92c31fdacbe71",
+    "url": "/Live-IPL-Score/static/js/runtime-main.fca33c29.js"
+  },
+  {
+    "revision": "377707a562f4101d5bb8e76d0574d2ac",
+    "url": "/Live-IPL-Score/static/media/vs.377707a5.png"
+  }
+]);
